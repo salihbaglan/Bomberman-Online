1c1ac67a6c91265c4c98972e3c922f6b6bd87666
salihbaglan <Salihbaglan36@gmail.com>
2023-05-22 15:12:40 +0300

    Proje gite eklendi

---
+++ Assets/unity-bomberman-tutorial-main/Assets/Scenes/SpawnPlayers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public Transform[] spawnPoints;
+
+    private void Start()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+        Vector3 randomPosition = spawnPoints[randomIndex].position;
+        PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
+    }
+}
